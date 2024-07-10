05deac91ceb0fde8eb15909758e282fbea0d6f48
Boburjon <soliyevboburjon95@gmail.com>
2024-07-10 20:06:41 +0500

    last

--- src/main/java/com/example/jpa_specification_example/exception/ApiException.java
+++ src/main/java/com/example/jpa_specification_example/exception/ApiException.java
@@ -3,8 +3,10 @@ package com.example.jpa_specification_example.exception;
 import com.example.jpa_specification_example.model.common.ResponseCodes;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ApiException extends RuntimeException {
     private String responseCode= ResponseCodes.SERVER_ERROR;
 
